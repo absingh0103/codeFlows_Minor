aa623215d6b7289a31a210fa5bf7ce2f101444ea
absingh0103 <abhiishek0103@gmail.com>
2024-10-22 23:21:27 +0530

    compiler_and_editor_integrated

---
+++ src/Version.js
@@ -0,0 +1,11 @@
+
+const VERSION = {
+    "javascript": '18.15.0',
+    "c": '10.2.0',
+    "c++": '10.2.0',
+    "go": '1.16.2',
+    "java": '15.0.2',
+    "python": '3.10.0',
+};
+
+module.exports = VERSION;
---
+++ src/socket.js
@@ -0,0 +1,18 @@
+// Socket initail Connection
+import { io } from 'socket.io-client';
+
+
+// Now When we Call this Function a New Client Instance is created and Returned 
+// then We can Emit and Get The data
+export const initSocket = async () => {
+    // basic options from socket io website for socket Connection
+    const options = {
+        'force new connection': true,
+        reconnectionAttempt: 'Infinity',
+        timeout: 10000,
+        transports: ['websocket'],
+    };
+    // this return the instance of A client 
+    return io("http://localhost:5000", options);
+};
+// process.env.REACT_APP_BACKEND_URL
